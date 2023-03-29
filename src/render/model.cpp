5f70f65de25639a4ec436897839a88494ee60df5
racenis <martinsrasaolaslasa@gmail.com>
2023-03-29 04:54:39 +0300

    Rewrote dynamic model loader

--- src/render/model.cpp
+++ src/render/model.cpp
@@ -4,6 +4,7 @@
 #include <framework/core.h>
 #include <framework/stats.h>
 #include <framework/file.h>
+#include <framework/logging.h>
 
 #include <render/model.h>
 #include <render/renderer.h>
@@ -104,15 +105,6 @@ bool Model::Unload(){
 }
 
 
-
-// TODO: make bucketing system better
-// right now you can only have one bucket per material
-// you should be able to have multiple buckets of the same material,
-// so that you can have more than 15 materials of the same type in a model
-
-// also you should have the same logic for both model types, instead of those huge branches
-
-// and iostream is kinda slow, so a better file reading thing should be used
 void Model::LoadFromDisk(){
     assert(status == UNLOADED);
     std::ifstream file;
@@ -274,8 +266,7 @@ void Model::LoadFromDisk(){
     strcat(path, name);
     strcat(path, ".dymdl");
 
-    file.open(path);
-    if (file.is_open()) {
+    if (File file (path, MODE_READ); file.is_open()) {
         vertex_format = DYNAMIC_VERTEX;
         DynamicModelData* data = new DynamicModelData;
         model_data = data;
@@ -284,138 +275,166 @@ void Model::LoadFromDisk(){
 
         assert(data);
         
-        uint32_t f_version = 0;
-        char ver_name[20];
-        file >> ver_name;
-        if (strcmp(ver_name, "DYMDLv1") == 0) f_version = 1;
-
-        uint32_t vcount = 0;     // number of vertices
-        uint32_t tcount = 0;     // number of triangles
-        uint32_t mcount = 0;     // number of materials
-        uint32_t bcount = 0;     // number of bones
-        uint32_t gcount = 0;     // number of vertex groups
+        name_t file_version = file.read_name();
         
-        if (f_version == 1) file >> vcount;
-        else vcount = atoi(ver_name);
-        file >> tcount;
-        file >> mcount;
-        file >> bcount;
-        file >> gcount;
-        
-        
-
-        std::cout << "f1" << std::endl;
-
-        if(mcount > 15){
-            std::cout << "Too many materials in model: " << name << std::endl;
-            mcount = 15;
-        }
-
-        std::cout << "f2" << std::endl;
-
-        for(uint32_t i = 0; i < mcount; i++){
-            Material* mat;
-            std::string matName;
-            file >> matName;
-
-            mat = Material::Find(UID(matName));
-
-            materials.push_back(mat);
-
+        if (file_version != UID("DYMDLv1")) {
+            std::cout << "Model " << path << " is not using right DYMDLv1 version!" << std::endl;
+            std::cout << "Add \"DYMDLv1\" to file and also add bone roll to the bone definitions (0.0 to the end of lines), or reexport." << std::endl;
         }
         
-        std::cout << "f3 " << vcount << " aa " << sizeof(DynamicModelVertex) * vcount << std::endl;
         
-        for(uint32_t i = 0; i < vcount; i++){
-            DynamicModelVertex v;
-            file >> v.co.x;
-            file >> v.co.y;
-            file >> v.co.z;
-            file >> v.normal.x;
-            file >> v.normal.y;
-            file >> v.normal.z;
-            file >> v.tex.x;
-            file >> v.tex.y;
-            file >> v.bone.x;
-            file >> v.boneweight.x;
-            file >> v.bone.y;
-            file >> v.boneweight.y;
-            file >> v.bone.z;
-            file >> v.boneweight.z;
-            file >> v.bone.w;
-            file >> v.boneweight.w;
-            v.texture = 0;
-            data->vertices.push_back(v);
+        uint32_t vcount = file.read_uint32();   // number of vertices
+        uint32_t tcount = file.read_uint32();   // number of triangles
+        uint32_t mcount = file.read_uint32();   // number of materials
+        uint32_t bcount = file.read_uint32();   // number of bones
+        uint32_t gcount = file.read_uint32();   // number of vertex groups
+
+
+        for (uint32_t i = 0; i < mcount; i++) {
+            materials.push_back(Material::Find(file.read_name()));
         }
         
-        std::cout << "f4" << std::endl;
-        
-        for(uint32_t i = 0; i < tcount; i++){
-            Triangle t;
-            uint32_t matIndex;
-            Material::Type matType;
-            file >> t.indices.x;
-            file >> t.indices.y;
-            file >> t.indices.z;
-            file >> matIndex;
-
-            matType = materials[matIndex]->GetType();
-
-            if(matIndex > 15 - 1) matIndex = 0;
-            triangle_buckets[matType].triangles.push_back(t);
-
-            data->vertices[t.indices.x].texture = matIndex;
-            data->vertices[t.indices.y].texture = matIndex;
-            data->vertices[t.indices.z].texture = matIndex;
-        }
-        
-        std::cout << "f5" << std::endl;
-        
-        for(uint32_t i = 0; i < bcount; i++){
-            Bone b;
-            std::string bonename;
-
-            file >> bonename;
-            file >> b.parentIndex;
-            file >> b.head.x;
-            file >> b.head.y;
-            file >> b.head.z;
-            file >> b.tail.x;
-            file >> b.tail.y;
-            file >> b.tail.z;
-            if (f_version == 1) file >> b.roll;
-
-            b.name = UID(bonename);
-            armature.push_back(b);
-        }
-        
-        std::cout << "f6" << std::endl;
-        
-        for(uint32_t i = 0; i < gcount; i++){
-            std::string g;
-            file >> g;
-            data->groups.push_back(g);
-        }
-        file.close();
-        std::cout << "f7" << std::endl;
-
-        //data->indices.reserve(tcount);
-
-        for (size_t i = 0; i < 6; i++) {
-            if (triangle_buckets[i].triangles.size() > 0) {
-                ElementRange range;
-                range.material_type = (Material::Type)i;
-                range.material_count = materials.size();
-                for (uint32_t k = 0; k < 15; k++) range.materials[k] = k;
-                range.element_length = triangle_buckets[i].triangles.size();
-                range.element_offset = data->indices.size();
+        for (uint32_t i = 0; i < vcount; i++) {
+            DynamicModelVertex vertex {
+                .co = {
+                    file.read_float32(),
+                    file.read_float32(),
+                    file.read_float32()
+                },
                 
-                element_ranges.push_back(range);
-                data->indices.insert(data->indices.end(), triangle_buckets[i].triangles.begin(), triangle_buckets[i].triangles.end());
-            }
-        }
+                .normal = {
+                    file.read_float32(),
+                    file.read_float32(),
+                    file.read_float32()
+                },
+                
+                .tex = {
+                    file.read_float32(),
+                    file.read_float32()
+                },
+                
+                // this looks stupid, but its because I accidentally made the
+                // model format stupid. maybe will be fixed in DYMDLv2
+                .bone = {0, 0, 0, 0},
+                
+                .boneweight = {0.0f, 0.0f, 0.0f, 0.0f},
+    
+                .texture = 0 // will be filled in later
+            };
+            
+            vertex.bone.x = file.read_uint32();
+            vertex.boneweight.x = file.read_float32();
+            vertex.bone.y = file.read_uint32();
+            vertex.boneweight.y = file.read_float32();
+            vertex.bone.z = file.read_uint32();
+            vertex.boneweight.z = file.read_float32();
+            vertex.bone.w = file.read_uint32();
+            vertex.boneweight.w = file.read_float32();
 
-std::cout << "f" << std::endl;
+            data->vertices.push_back(vertex);
+            
+            if (i % 100 == 0) std::cout << i << std::endl;
+        }
+        
+        // this is basically a repetion of the same code as for static model
+        // so, it could be possible to make this a function.
+        for (uint32_t i = 0; i < tcount; i++) {
+            Triangle index {
+                .indices = {
+                    file.read_uint32(),
+                    file.read_uint32(),
+                    file.read_uint32()
+                }
+            };
+            
+            uint32_t material_index = file.read_uint32();
+            Material::Type material_type = materials[material_index]->GetType();
+            
+            size_t bucket = 0;
+            
+            // try to find the bucket that contains this triangle's material
+            for (; bucket < triangle_buckets.size(); bucket++) {
+                if (triangle_buckets[bucket].material_type != material_type) continue;
+                
+                // check if material is in the bucket
+                for (size_t i = 0; i < triangle_buckets[bucket].materials.size(); i++) {
+                    if (triangle_buckets[bucket].materials[i] == material_index) {
+                        goto found_bucket2;
+                    }
+                }
+                
+                // otherwise try to add it to the bucket
+                if (triangle_buckets[bucket].materials.size() < 15) {
+                    triangle_buckets[bucket].materials.push_back(material_index);
+                    goto found_bucket2;
+                }
+            }            
+            
+            found_bucket2:
+
+            if (bucket < triangle_buckets.size()) {
+                triangle_buckets[bucket].triangles.push_back(index);
+            } else {
+                assert(triangle_buckets.size() < 3);
+                
+                triangle_buckets.push_back(TriangleBucket {
+                    .material_type = material_type,
+                    .materials = {material_index},
+                    .triangles = {index}
+                });
+            }
+
+            data->vertices[index.indices.x].texture = material_index;
+            data->vertices[index.indices.y].texture = material_index;
+            data->vertices[index.indices.z].texture = material_index;
+        }
+        
+        for (uint32_t i = 0; i < bcount; i++) {
+            armature.push_back(Bone {
+                .name = file.read_name(),
+                .parentIndex = file.read_uint32(),
+                
+                .head = {
+                    file.read_float32(),
+                    file.read_float32(),
+                    file.read_float32()
+                },
+                
+                .tail = {
+                    file.read_float32(),
+                    file.read_float32(),
+                    file.read_float32()
+                },
+                
+                .roll = file.read_float32()
+            });
+        }
+        
+        for (uint32_t i = 0; i < gcount; i++) {
+            name_t group = file.read_name();
+            
+            if (armature[i].name != group) {
+                std::cout << "Model " << name << " group " << group << " is not matching bone " << armature[i].name << "!" << std::endl;
+            }
+            
+            data->groups.push_back(group);
+        }
+        
+        for (auto& bucket : triangle_buckets) {
+            ElementRange range {
+                .element_offset = (uint32_t) data->indices.size(),
+                .element_length = (uint32_t) bucket.triangles.size(),
+                .material_count = (uint32_t) bucket.materials.size(),
+                .material_type = bucket.material_type,
+            };
+            
+            for (size_t i = 0; i < bucket.materials.size(); i++) {
+                range.materials[i] = bucket.materials[i];
+            }
+            
+            element_ranges.push_back(range);
+            data->indices.insert(data->indices.end(), bucket.triangles.begin(), bucket.triangles.end());
+        }
 
         // push references into material
         status = LOADED;
@@ -432,7 +451,7 @@ std::cout << "f" << std::endl;
 
     //status = LOAD_FAIL;
 
-        // if we get this far, then it means that the model wasn't loaded from disk
+    // if we get this far, then it means that the model wasn't loaded from disk
 
     std::cout << "Model file for " << name << " couldn't be accessed!" << std::endl;
 
