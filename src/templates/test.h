c76ca8b3edb310e41b24876b9a10b6640c5b9f39
racenis <martinsrasaolaslasa@gmail.com>
2026-02-07 21:44:30 +0200

    Added time measurements to test framework

--- src/templates/test.h
+++ src/templates/test.h
@@ -5,6 +5,7 @@
 
 #include <cstdio>
 #include <cstring>
+#include <chrono>
 
 // This is the framwork for testing the library.
 // It not actually part of library. But only test. I am testing.
@@ -16,6 +17,37 @@ struct _test_case {
     int totl_asserts = 0;
     int fail_asserts = 0;
     
+    static constexpr std::chrono::time_point<std::chrono::high_resolution_clock> INVALID_TIME = {};
+    
+    std::chrono::time_point<std::chrono::high_resolution_clock> times_starts[10];
+    std::chrono::time_point<std::chrono::high_resolution_clock> times_ends[10];
+    const char* times_names[10];
+    int totl_times = 0;
+    
+    void timestart(const char* name) {
+        times_starts[totl_times] = std::chrono::high_resolution_clock::now();
+        times_ends[totl_times] = INVALID_TIME;
+        times_names[totl_times] = name;
+        
+        totl_times++;
+    }
+    
+    void timefinish(const char* name) {
+        int idx = 0;
+        while (idx < 10 && strcmp(times_names[idx], name) != 0) idx++;
+        if (idx >= 10) {
+            printf("Forgot to call TIME_START(\"%s\")!\n", name);
+            return;
+        }
+        
+        if (times_ends[idx] != INVALID_TIME) {
+            printf("Called TIME_FINISH(\"%s\") twice!\n", name);
+            return;
+        }
+        
+        times_ends[idx] = std::chrono::high_resolution_clock::now();
+    }
+    
     void fail() {
         totl_asserts++;
         fail_asserts++;
@@ -24,6 +56,37 @@ struct _test_case {
     void succ() {
         totl_asserts++;
     }
+    
+    void print() {
+        int p = totl_asserts - fail_asserts;
+        int t = totl_asserts;
+        
+        printf("Test %s %s (%i/%i)\n", fullname, abbrv, p, t);
+        
+
+        decltype(*times_ends - *times_starts) time_sum = {};
+        for (int i = 0; i < totl_times; i++) {
+            if (times_ends[i] == INVALID_TIME) {
+                printf("Forgot to call TIME_FINISH(\"%s\") twice!\n", times_names[i]);
+                continue;
+            }
+            auto time = times_ends[i] - times_starts[i];
+            time_sum += time;
+            auto print_time = std::chrono::duration_cast<std::chrono::milliseconds>(time).count();
+            char pad[80] = "";
+            int pads = 32 - strlen(times_names[i]);
+            for (int j = 0; j < pads; j++) strcat(pad, " ");
+            
+            printf("- %s%s%ims\n", times_names[i], pad, print_time);
+        }
+        
+        if (totl_times > 1) {
+            auto print_time = std::chrono::duration_cast<std::chrono::milliseconds>(time_sum).count();
+            printf("- Total                           %ims\n", print_time);
+        }
+        
+    }
+    
 };
 
 static _test_case _all_test_cases[100];
@@ -36,7 +99,11 @@ static _test_case* _last_test_case = nullptr;
 
 
 
-
+#define TIME_START(NAME) \
+    _last_test_case->timestart(NAME);
+    
+#define TIME_FINISH(NAME) \
+    _last_test_case->timefinish(NAME);
 
 #define TEST_CASE(X, Y) \
     _last_test_case = &_all_test_cases[_test_case_count]; \
@@ -50,25 +117,22 @@ static _test_case* _last_test_case = nullptr;
     int main(int argc, char** argv) {
         
 #define END_TEST \
-    bool fail = false;\
-\
-printf("\nRESULTS:\n");\
-\
-    for (int i = 0; i < _test_case_count; i++) {\
-        auto c = &_all_test_cases[i];\
+        bool fail = false;\
         \
-        if (c->fail_asserts > 0) {\
-            fail = true;\
+        printf("\nRESULTS:\n");\
+        \
+        for (int i = 0; i < _test_case_count; i++) {\
+            auto c = &_all_test_cases[i];\
+            \
+            if (c->fail_asserts > 0) {\
+                fail = true;\
+            }\
+            \
+            c->print(); \
         }\
         \
-        int p = c->totl_asserts - c->fail_asserts;\
-        int t = c->totl_asserts;\
-        \
-        printf("Test %s %s (%i/%i)\n", c->fullname, c->abbrv, p, t);\
-    }\
-\
-    return fail ? -1 : 0;\
-}
+        return fail ? -1 : 0;\
+    }
 
 
 #endif  // TRAM_SDK_TEMPLATES_TEST_H
