41b68c5160acaab968c94b60081cbefe800cf875
racenis <martinsrasaolaslasa@gmail.com>
2026-06-08 07:47:52 +0300

    Added Environment map resource

---
+++ src/render/environment.cpp
@@ -0,0 +1,187 @@
+// Tramway Drifting and Dungeon Exploration Simulator SDK Runtime
+
+#define STBI_ONLY_PNG
+#include <stb_image.h>
+
+#include <framework/stats.h>
+#include <framework/logging.h>
+
+#include <render/environment.h>
+#include <render/api.h>
+
+#include <platform/file.h>
+
+#include <templates/hashmap.h>
+
+#include <config.h>
+
+/**
+ * @class tram::Render::Environment render/environment.h <render/environment.h>
+ * 
+ * Environment map resource.
+ * 
+ * @see https://racenis.github.io/tram-sdk/documentation/render/environment.html
+ */
+
+using namespace tram;
+
+template <> Pool<Render::Environment> PoolProxy<Render::Environment>::pool("Environment map pool", RESOURCE_LIMIT_ENVIRONMENTMAP);
+
+namespace tram::Render {
+
+static Hashmap<Environment*> environment_name_list("Environment map name list", RESOURCE_LIMIT_ENVIRONMENTMAP);
+static Hashmap<std::vector<Environment*>*> environment_index_list("Environment map index list", RESOURCE_LIMIT_ENVIRONMENTMAP);
+
+/// Finds an Environment map.
+/// Finds an Environment map by its associated name. If a environment map with
+/// that name does not exist, it will be created.
+/// @return Always returns a pointer to a Environment map.
+Environment* Environment::Find(name_t name) {
+    Environment* environment = environment_name_list.Find(name);
+    
+    if (!environment) {
+        environment = PoolProxy<Environment>::GetPool().allocate();
+        new(environment) Environment(name);
+        environment_name_list.Insert(name, environment);
+    }
+    
+    return environment;
+}
+
+/// Finds an Environment map.
+/// Finds an Environment map by its associated index and graph. If a environment
+/// map with that index does not exist, it will be created.
+/// @return Always returns a pointer to a Environment map.
+Environment* Environment::Find(name_t graph, uint32_t index) {
+    std::vector<Environment*>* graphmaps = environment_index_list.Find(graph);
+    if (!graphmaps) {
+        graphmaps = new std::vector<Environment*>;
+        environment_index_list.Insert(graph, graphmaps);
+    }
+    
+    if (index >= graphmaps->size()) {
+        graphmaps->resize(index + 1, nullptr);
+    }
+    
+    Environment* environment = (*graphmaps)[index];
+    
+    if (!environment) {
+        environment = PoolProxy<Environment>::GetPool().allocate();
+        new(environment) Environment(graph, index);
+        (*graphmaps)[index] = environment;
+    }
+    
+    return environment;
+}
+
+static uint32_t layer_count_for_type(EnvironmentType type) {
+    switch (type) {
+        case ENVIRONMENT_SPHERE:    return 2;
+        default:                    return 1;
+    }
+}
+
+void Environment::LoadFromDisk() {
+    assert(status == UNLOADED);
+    
+    uint32_t layer_count = layer_count_for_type(type);
+    
+    int loadwidth, loadheight, loadchannels;
+    unsigned char* loadtexture = nullptr;
+    char path[100];
+  
+    for (uint32_t layer = 0; layer < layer_count; layer++) {
+        if (name) {
+            snprintf(path, 100, "data/environments/%s.%i.png", (const char*)name, layer);
+        } else {
+            snprintf(path, 100, "data/environments/%s.%i.%i.png", (const char*)graph, index, layer);
+        }
+        
+        FileReader* file = FileReader::GetReader(path);
+        if (file->GetStatus() == FileStatus::READY) {
+            stbi_set_flip_vertically_on_load(true);
+            loadtexture = stbi_load_from_memory((const unsigned char*)file->GetContents(), file->GetSize(), &loadwidth, &loadheight, &loadchannels, 3);
+
+            if (texture_data && (loadwidth != (int)width || loadheight != (int)height)) {
+                std::cout << "Environment map" << (name ? std::string(name) : std::string(graph) + std::to_string(index)) << " layer " << layer << " dimensions of " << loadwidth << " by " << loadheight << " don't match first frame dimensions of " << width << " by " << height << std::endl;
+                stbi_image_free(loadtexture);
+                loadtexture = nullptr;
+            }
+        }
+        file->Yeet();
+        
+        if (!texture_data) {
+            if (loadtexture) {
+                width = loadwidth;
+                height = loadheight;
+            } else {
+                width = 64;
+                height = 64;
+            }
+            
+            texture_data = new uint8_t[width * height * 3 * layer_count];
+        }
+        
+        if (loadtexture) {
+            memcpy(texture_data + width * height * 3 * layer, loadtexture, width * height * 3);
+            stbi_image_free(loadtexture);
+            loadtexture = nullptr;
+        } else {
+            std::cout << "Environment map " << (name ? std::string(name) : std::string(graph) + std::to_string(index)) << " (" << path << ") couldn't be loaded!" << std::endl;
+            
+            load_fail = true;
+            
+            // generate error pattern
+            for (uint32_t x = 0; x < width; x++)
+            for (uint32_t y = 0; y < height; y++) {
+                uint32_t offset = (x + y * width + width * height * layer) * 3;
+                bool w = x & 16;
+                bool h = y & 16;
+                uint8_t color = w ^ h ? 255 : 0;
+                texture_data[offset + 0] = color;
+                texture_data[offset + 1] = color;
+                texture_data[offset + 2] = color;
+            }
+        }
+    }
+    
+    status = LOADED;
+}
+
+void Environment::LoadFromMemory() {
+    assert(status == LOADED);
+
+    // TODO: add a check that this is being called from render thread
+
+    texture = API::CreateTexture(COLORMODE_RGB, TEXTUREFILTER_LINEAR, width, height, layer_count_for_type(type), texture_data);
+    
+    float approx_memory = 3.0f * width * height * layer_count_for_type(type);  // image size
+    approx_memory = approx_memory * 1.3f;             // plus mipmaps
+    
+    approx_vram_usage = (size_t) approx_memory;
+    
+    Stats::Add(Stats::RESOURCE_VRAM, approx_vram_usage);
+
+    delete[] texture_data;
+    texture_data = nullptr;
+    
+    status = READY;
+    return;
+}
+
+void Environment::Unload() {
+    assert(status == READY);
+    
+    // TODO: add a check that this is being called from render thread
+    
+    if (texture.generic) API::YeetTexture(texture);
+    
+    texture.generic = nullptr;
+
+    status = UNLOADED;
+}
+
+}
+
+
+
---
+++ src/render/environment.h
@@ -0,0 +1,49 @@
+// Tramway Drifting and Dungeon Exploration Simulator SDK Runtime
+
+#ifndef TRAM_SDK_RENDER_LIGHTMAP_H
+#define TRAM_SDK_RENDER_LIGHTMAP_H
+
+#include <render/render.h>
+
+namespace tram::Render {
+
+enum EnvironmentType {
+    ENVIRONMENT_SPHERE,
+};
+
+class Environment : public Resource {
+public:
+    inline texturearray_t GetTexture() const { return texture; }
+    inline uint32_t GetWidth() const { return width; }
+    inline uint32_t GetHeight() const { return height; }
+    inline EnvironmentType GetType() const { return type; }
+
+    inline void SetType(EnvironmentType type) { this->type = type; }
+
+    void LoadFromDisk();
+    void LoadFromMemory();
+    
+    void Unload();
+    
+    static Environment* Find(name_t graph, uint32_t index);
+    static Environment* Find(name_t name);
+protected:
+    Environment(name_t name) : Resource(name) {}
+    Environment(name_t graph, uint32_t index) : Resource(std::to_string(index)) {} // TODO: fix
+    
+    texturearray_t texture = {};
+
+    EnvironmentType type = ENVIRONMENT_SPHERE;
+    
+    name_t graph;
+    uint32_t index = 0;
+    
+    uint32_t width = 0;
+    uint32_t height = 0;
+    uint8_t* texture_data = nullptr;
+    size_t approx_vram_usage = 0;
+};
+
+}
+
+#endif // TRAM_SDK_RENDER_LIGHTMAP_H
