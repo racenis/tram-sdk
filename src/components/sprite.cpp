1ea06371c10b102d18df0fe0cbe9fd39bc2cc020
racenis <martinsrasaolaslasa@gmail.com>
2026-07-10 01:12:33 +0300

    Added multiple sprites per SpriteComponent

--- src/components/sprite.cpp
+++ src/components/sprite.cpp
@@ -8,6 +8,9 @@
 
 #include <config.h>
 
+#include <cstdlib>
+#include <cstring>
+
 /**
  * @class tram::SpriteComponent components/sprite.h <components/sprite.h>
  * 
@@ -26,9 +29,56 @@ using namespace API;
 
 SpriteComponent::~SpriteComponent() {
     is_ready = false;
-
+    
+    if (infos) free(infos);
+    if (points) free(points);
+    
     RemoveDrawListEntry(draw_list_entry);
 };
+
+void SpriteComponent::SetSprite(Render::Sprite* sprite) {
+    if (is_ready) {
+        Log(Severity::WARNING, System::RENDER, "Initialized SpriteComponents cannot accept sprites! Ignoring SpriteComponent::SetSprite() call.");
+        return;
+    }
+    
+    this->sprite = sprite;    
+}
+
+void SpriteComponent::Resize(int32_t frame, bool downside) {
+    int32_t want_count = frame + 1;
+    if (sprite_count == want_count) return;
+    
+    if (!sprite_count && want_count > 0) {
+        points = (Render::SpritePoint*)calloc(want_count, sizeof(Render::SpritePoint));
+        infos = (SpriteInfo*)calloc(want_count, sizeof(SpriteInfo));
+        sprite_count = want_count;
+        for (int32_t i = 0; i < want_count; i++) {
+            infos[i].scale = {1.0f, 1.0f};
+        }
+        return;
+    }
+    
+    if (want_count < 1) {
+        if (points) free(points);
+        if (infos) free(infos);
+        sprite_count = 0;
+        return;
+    }
+    
+    points = (Render::SpritePoint*)realloc(points, want_count * sizeof(Render::SpritePoint));
+    infos = (SpriteInfo*)realloc(infos, want_count * sizeof(SpriteInfo));
+    
+    if (want_count > sprite_count) {
+        memset(points + sprite_count, 0, (want_count - sprite_count) * sizeof(Render::SpritePoint));
+        memset(infos + sprite_count, 0, (want_count - sprite_count) * sizeof(SpriteInfo));
+        for (int32_t i = sprite_count; i < want_count; i++) {
+            infos[i].scale = {1.0f, 1.0f};
+        }
+    }
+    
+    sprite_count = want_count;
+}
 
 void SpriteComponent::Start() {
     if (!sprite) {
@@ -42,6 +92,7 @@ void SpriteComponent::Start() {
     draw_list_entry = InsertDrawListEntry();
     SetDrawListSpriteArray(draw_list_entry, sprite_array);
     SetDrawListIndexRange(draw_list_entry, 0, 6);
+    SetLayer(draw_list_entry, layers);
     SetFlags(draw_list_entry, FLAG_RENDER);
     SetDrawListMaterials(draw_list_entry, 1, &texture_handle);
     SetDrawListShader(draw_list_entry, VERTEX_SPRITE, MATERIAL_TEXTURE_ALPHA);
@@ -53,59 +104,58 @@ void SpriteComponent::Start() {
 void SpriteComponent::Update() {
     if (!is_ready) return;
     
-    if (anim_isplaying) {
-        UpdateRenderListObject();
-    
-        if (!(anim_bframe < anim_speed)) {
-            anim_bframe = 0;
-            anim_frame++;
-            
-            if (!(anim_frame < sprite->GetFrames().size())) anim_frame = 0;
+    if (sprites_playing) {
+        for (int32_t i = 0; i < sprite_count; i++) {
+            if (++infos[i].since_last_anim_frame > infos[i].anim_speed) {
+                infos[i].since_last_anim_frame = 0;
+                infos[i].anim_frame++;
+                if (infos[i].anim_frame >= infos[i].last_frame) {
+                    infos[i].anim_frame = infos[i].first_frame;
+                }
+                renderlist_needs_update = true;
+            }
         }
-        anim_bframe++;
+    }
+    
+    if (renderlist_needs_update) {
+        UpdateRenderListObject();
     }
 }
 
 void SpriteComponent::UpdateRenderListObject() {
     if (!is_ready) return;
-
-    // maybe cache these values, instead of re-calculating them for each frame?
-    float tex_width = (float)sprite->GetFrames()[anim_frame].width / (float)sprite->GetMaterial()->GetWidth();
-    float tex_height = (float)sprite->GetFrames()[anim_frame].height / (float)sprite->GetMaterial()->GetHeight();
-    float tex_w_off = (float)sprite->GetFrames()[anim_frame].offset_x / (float)sprite->GetMaterial()->GetWidth();
-    float tex_h_off = (float)sprite->GetFrames()[anim_frame].offset_y / (float)sprite->GetMaterial()->GetHeight();
     
-    float tex_x_mid = (float)sprite->GetFrames()[anim_frame].midpoint_x / (float)sprite->GetMaterial()->GetWidth();
-    float tex_y_mid = (float)sprite->GetFrames()[anim_frame].midpoint_y / (float)sprite->GetMaterial()->GetHeight();
-
-    tex_h_off = 1.0f - tex_h_off - tex_height;
-
-    Render::SpritePoint point;
-    point.position = {0.0f, 0.0f, 0.0f};
-    point.color = COLOR_WHITE;
-    point.rotation = rotation;
-    point.dimensions = {tex_width * 10.0f, tex_height * 10.0f}; // ?? what in 
-    point.midpoint = {tex_x_mid * 10.0f, tex_y_mid * 10.0f}; // yeah idk
-    point.texture_offset = {tex_w_off, tex_h_off};
-    point.texture_size = {tex_width, tex_height};
-    point.texture = 0;
-
-    UpdateSpriteArray(sprite_array, 1, &point);
+    vec3 midpoint = {0.0f, 0.0f, 0.0f};
+    for (int32_t i = 0; i < sprite_count; i++) {
+        midpoint += infos[i].location;
+    }
+    if (sprite_count) midpoint /= sprite_count;
     
-    Render::API::SetMatrix(draw_list_entry, PositionRotationToMatrix(location, quat(1.0f, 0.0f, 0.0f, 0.0f)));
-}
-
+    for (int32_t i = 0; i < sprite_count; i++) {
+        float tex_width = (float)sprite->GetFrames()[infos[i].anim_frame].width / (float)sprite->GetMaterial()->GetWidth();
+        float tex_height = (float)sprite->GetFrames()[infos[i].anim_frame].height / (float)sprite->GetMaterial()->GetHeight();
+        float tex_w_off = (float)sprite->GetFrames()[infos[i].anim_frame].offset_x / (float)sprite->GetMaterial()->GetWidth();
+        float tex_h_off = (float)sprite->GetFrames()[infos[i].anim_frame].offset_y / (float)sprite->GetMaterial()->GetHeight();
         
-void SpriteComponent::Play() {
-    anim_isplaying = true;
-}
+        float wrld_x_mid = (float)sprite->GetFrames()[infos[i].anim_frame].midpoint_x / 32.0f;
+        float wrld_y_mid = (float)sprite->GetFrames()[infos[i].anim_frame].midpoint_y / 32.0f;
+        
+        float wrld_width = (float)sprite->GetFrames()[infos[i].anim_frame].width / 32.0f;
+        float wrld_height = (float)sprite->GetFrames()[infos[i].anim_frame].height / 32.0f;
 
-void SpriteComponent::Pause() {
-    anim_isplaying = true;
-}
+        tex_h_off = 1.0f - tex_h_off - tex_height;
+        
+        points[i].position = infos[i].location - midpoint;
+        points[i].dimensions = {wrld_width * infos[i].scale.x, wrld_height * infos[i].scale.y};
+        points[i].midpoint = {wrld_x_mid * infos[i].scale.x, wrld_y_mid * infos[i].scale.y};
+        points[i].texture_offset = {tex_w_off, tex_h_off};
+        points[i].texture_size = {tex_width, tex_height};
+    }
 
-void SpriteComponent::SetPlaySpeed(size_t speed) {
-    anim_speed = speed;
+    UpdateSpriteArray(sprite_array, sprite_count, points);
+    
+    SetDrawListIndexRange(draw_list_entry, 0, sprite_count * 6);
+    Render::API::SetMatrix(draw_list_entry, PositionRotationToMatrix(midpoint, quat(1.0f, 0.0f, 0.0f, 0.0f)));
 }
 
 /// Creates a new SpriteComponent.
--- src/components/sprite.h
+++ src/components/sprite.h
@@ -6,6 +6,8 @@
 #include <render/render.h>
 #include <render/sprite.h>
 
+#include <render/vertices.h>
+
 namespace tram {
 
 class SpriteComponent : public EntityComponent {
@@ -14,39 +16,104 @@ public:
     static void Yeet(SpriteComponent* component);
     
     inline Render::Sprite* GetSprite() const { return sprite.get(); }
+    void SetSprite(Render::Sprite* sprite);
 
-    void SetSprite(Render::Sprite* sprite){
-        this->sprite = sprite;
-    }
-    
     void Update();
     
-    void Play();
-    void Pause();
+    inline void SetScale(float scale, int32_t index = 0) {
+        Resize(index); infos[index].scale = {scale, scale};
+        renderlist_needs_update = true;
+    }
     
-    void SetPlaySpeed(size_t speed);
+    inline void SetScale(vec2 scale, int32_t index = 0) {
+        Resize(index); infos[index].scale = scale;
+        renderlist_needs_update = true;
+    }
+    
+    inline void SetFrame(int32_t frame, int32_t index = 0) {
+        Resize(index); infos[index].anim_frame = frame;
+        renderlist_needs_update = true;
+    }
+    
+    inline void SetSpeed(int32_t speed, int32_t index = 0) {
+        Resize(index); infos[index].anim_speed = speed;
+        renderlist_needs_update = true;
+    }
+    
+    inline void SetFrameBegin(int32_t frame, int32_t index = 0) {
+        Resize(index); infos[index].first_frame = frame;
+        renderlist_needs_update = true;
+    }
+    
+    inline void SetFrameEnd(int32_t frame, int32_t index = 0) {
+        Resize(index); infos[index].last_frame = frame;
+        renderlist_needs_update = true;
+    }
+    
+    inline void Play(int32_t index = 0) {
+        Resize(index);
+        if (!infos[index].is_playing) sprites_playing++;
+        infos[index].is_playing = true;
+    }
+    
+    inline void Pause(int32_t index = 0) {
+        Resize(index);
+        if (infos[index].is_playing) sprites_playing--;
+        infos[index].is_playing = false;
+    }
+  
+    void SetColor(vec3 color, int32_t index = 0) {
+        Resize(index); points[index].color = color;
+        renderlist_needs_update = true;
+    }
 
-    void UpdateLocation(vec3 nlocation){
-        location = nlocation;
-        UpdateRenderListObject();
+    inline void SetLocation(vec3 location, int32_t index = 0) {
+        Resize(index); infos[index].location = location;
+        renderlist_needs_update = true;
     }
     
-    void SetRotation(float rotation) {
-        this->rotation = rotation;
-        UpdateRenderListObject();
+    inline void SetRotation(float rotation, int32_t index = 0) {
+        Resize(index); points[index].rotation = rotation;
+        renderlist_needs_update = true;
     }
+    
+    void SetLayers(uint32_t layers) {
+        this->layers = layers;
+        renderlist_needs_update = true;
+    }
+    
+    int32_t GetSpriteCount() { return sprite_count; }
+    
+    inline void SetSpriteCount(int32_t count) { Resize(count - 1, true); }
 
     void EventHandler(Event &event){return;}
 protected:
     ResourceProxy<Render::Sprite> sprite;
-
-    vec3 location = {0.0f, 0.0f, 0.0f};
-    float rotation = 0.0f;
+    uint32_t layers = 1;
     
-    size_t anim_frame = 0;
-    size_t anim_speed = 0;
-    size_t anim_bframe = 0;
-    bool anim_isplaying = false;
+    struct SpriteInfo {
+        int32_t anim_frame;
+        int32_t anim_speed;
+        
+        int32_t first_frame;
+        int32_t last_frame;
+        
+        vec3 location;
+        vec2 scale;
+        
+        bool is_playing;
+        
+        int32_t since_last_anim_frame;
+    };
+    
+    Render::SpritePoint* points = nullptr;
+    SpriteInfo* infos = nullptr;
+    int32_t sprite_count = 0;
+    int32_t sprites_playing = 0;
+    
+    void Resize(int32_t frame, bool downside = false);
+    
+    bool renderlist_needs_update = false;
     
     void Start();
     void UpdateRenderListObject();
